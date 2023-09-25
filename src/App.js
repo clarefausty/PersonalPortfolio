1c15936a8ac7b0142efc89d13be20b338de7b1e3
clarefausty <clarefausty@gmail.com>
2023-09-25 12:17:23 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import About from './components/about/About';
+import Head from './components/header/Head';
+
+function App() {
+  return (
+    <div className="App">
+      <Head/>
+      <About/>
+    </div>
+  );
+}
+
+export default App;
