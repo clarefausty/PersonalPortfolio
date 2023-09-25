3241514aec438751dcbec4464f26f53334322a0c
clarefausty <clarefausty@gmail.com>
2023-09-25 14:56:52 +0100

    wip

--- src/components/header/Head.js
+++ src/components/header/Head.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import CV from "../../assets/Resume.pdf"
 import "./head.css"
+import {BsGithub} from "react-icons/bs"
+import {AiFillTwitterSquare} from "react-icons/ai"
+import {AiFillLinkedin} from "react-icons/ai"
 
 function Head() {
   return (
@@ -11,6 +14,13 @@ function Head() {
         <div className='head-btn' >
             <a download className='cv-btn' href={CV}>Download CV</a>
             <a className='talk-btn' href='#contact'>Let's Talk</a>
+        </div>
+        <div className='head-section2'>
+            <div>
+                <BsGithub/>
+                <AiFillTwitterSquare/>
+                <AiFillLinkedin/>
+            </div>
         </div>
     </div>
   )
