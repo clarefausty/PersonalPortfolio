a061aaac72b574026a06321998e8dc0b163d29aa
clarefausty <clarefausty@gmail.com>
2023-09-26 03:11:13 +0100

    wip

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import ME from "../../assets/ME-removebg-preview.png"
+import {FaAward} from "react-icons/fa"
+import {PiProjectorScreenChartFill} from "react-icons/pi"
 
 function About() {
   return (
-    <div>About</div>
+    <section id='about'>
+        <h5>Get to know</h5>
+        <h4>About Me</h4>
+        <div className='about-container'>
+            <div className='about-me'>
+                <img src={ME} alt='Me'/>
+            </div>
+            <div className='about-content'>
+            <div className='about-cards'>
+                <article className='about-card'>
+                <FaAward/>
+                <h5>Experience</h5>
+                <small>2+ years working</small>
+                </article>
+                <article className='about-card'>
+                <PiProjectorScreenChartFill/>
+                <h5>Projects</h5>
+                </article>
+            </div>
+            <p>Dedicated and skilled Frontend Developer with a strong focus on JavaScript and React.js. Proficient in creating dynamic and responsive user interfaces, I bring creativity and technical expertise to web development projects. Experienced in collaborating with cross-functional teams to deliver high-quality, user-centric solutions. Eager to contribute my frontend development skills to drive innovation and enhance user experiences in the digital landscape.</p>
+            <div>
+                <button>Let's Talk</button>
+            </div>
+            </div>
+        </div>
+
+    </section>
   )
 }
 
