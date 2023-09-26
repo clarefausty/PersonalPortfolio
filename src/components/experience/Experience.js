920ecd9bfdf0441aca56da87214561b396cd1d99
clarefausty <clarefausty@gmail.com>
2023-09-26 14:48:58 +0100

    wip

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -11,38 +11,38 @@ import "./experience.css"
 
 function Experience() {
   return (
-    <section id='experience'>
+    <section className='container' id='experience'>
         <h2>Skills</h2>
         <div className='skills'>
             <div className='skill html'>
             <SiHtml5/>
             <h5>HTML</h5>
             </div>
-            <div className='skill html'>
+            <div className='skill css'>
             <PiFileCssFill/>
             <h5>CSS</h5>
             </div>
-            <div className='js-con'>
+            <div className='skill js'>
             <TbBrandJavascript/>
             <h5>JavaScript</h5>
             </div>
-            <div className='js-con'>
+            <div className='skill react'>
             <GrReactjs/>
             <h5>React</h5>
             </div>
-            <div className='js-con'>
+            <div className='skill tailwind'>
             <SiTailwindcss/>
             <h5>Tailwind CSS</h5>
             </div>
-            <div className='js-con'>
+            <div className='skill bootstrap'>
             <FaBootstrap/>
             <h5>Bootsrap</h5>
             </div>
-            <div className='js-con'>
+            <div className='skill git'>
             <BsGit/>
             <h5>Git</h5>
             </div>
-            <div className='js-con'>
+            <div className='skill github'>
             <BsGithub/>
             <h5>Github</h5>
             </div>
