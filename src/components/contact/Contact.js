eb21384c083f0a1699fed8a92a03f778c7a7e47b
clarefausty <clarefausty@gmail.com>
2023-09-26 17:05:43 +0100

    wip

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import {HiOutlineMail} from "react-icons/hi"
+import "./contact.css"
 
 function Contact() {
   return (
@@ -7,8 +9,17 @@ function Contact() {
     <h2>Contact Me</h2>
     <div className='contact-container'>
         <div className='contact-options'>
-        <article className='about-card'>
-
+        <article className='about-card contact-option'>
+        <HiOutlineMail/>
+        <h4>Email</h4>
+        <h5>clarefausty@gmail.com</h5>
+        <a href='mailto:clarefausty@gmail.com'>Send a Message</a>
+        </article>
+        <article className='about-card contact-option'>
+        <HiOutlineMail/>
+        <h4>Whatsapp</h4>
+        <h5>+2348169569392</h5>
+        <a href='https://api.whatsapp.com/send?phone+2348169569392'>Send a Message</a>
         </article>
         </div>
     <form>
