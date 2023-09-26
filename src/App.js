68c81dc7236f545040b9ef688f194bad289863a3
clarefausty <clarefausty@gmail.com>
2023-09-26 07:52:26 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import About from './components/about/About';
 import Head from './components/header/Head';
+import Nav from './components/nav/Nav';
 
 function App() {
   return (
     <div className="container-app">
       <Head/>
+      <Nav/>
       <About/>
     </div>
   );
---
+++ src/components/nav/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./nav.css"
+import {AiOutlineHome} from "react-icons/ai"
+import {AiOutlineUser} from "react-icons/ai"
+import {FaProjectDiagram} from "react-icons/fa"
+import {MdContactPhone} from "react-icons/md"
+
+function Nav() {
+  return (
+   <nav>
+    <a href='#'><AiOutlineHome/></a>
+    <a href='#about'><AiOutlineUser/></a>
+    <a href='#project'><FaProjectDiagram/></a>
+    <a href='contact#'><MdContactPhone/></a>
+   </nav>
+  )
+}
+
+export default Nav
